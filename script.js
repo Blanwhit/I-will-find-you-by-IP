7c4dc6124b07e14af44fa16f81e85978f1a23cc6
Black-n-White <94003181+Black-n-White@users.noreply.github.com>
2022-09-07 13:06:37 +0300

    fix

--- script.js
+++ script.js
@@ -18,30 +18,27 @@ const region = document.getElementById("region");
 const regionName = document.getElementById("regionName");
 const city = document.getElementById("city");
 const district = document.getElementById("district");
-const zip = document.getElementById("zip");
-const isp = document.getElementById("isp");
+const latitude = document.getElementById("zip");
+const longtitude = document.getElementById("isp");
 const query = document.getElementById("query");
 const weAreComing = document.getElementById("weAreComing")
 
 
 function getAdress() {
-    fetch(`https://ip-api.com/json/${userIp}?fields=status,message,continent,country,countryCode,region,regionName,city,district,zip,isp,query`)
+    fetch(`https://api.ip2loc.com/f2bxD65xrdvO4jlgoQn0E9XTNK3apDEH/${userIp}`)
         .then(response => response.json())
         .then(response => {
-            status.innerText = `status: ${response.status}`;
-            continent.innerText = `continent: ${response.continent}`;
-            country.innerText = `country: ${response.country}`;
-            countryCode.innerText = `countryCode: ${response.countryCode}`;
-            region.innerText = `region: ${response.region}`;
-            regionName.innerText = `regionName: ${response.regionName}`;
-            city.innerText = `city: ${response.city}`;
-            district.innerText = `district: ${response.district}`;
-            zip.innerText = `zip: ${response.zip}`;
-            isp.innerText = `provider: ${response.isp}`;
-            query.innerText = `ip: ${response.query}`;
-            if (response.message) {
-                message.innerText = `Error message: ${response.message}`;
-            }
+            status.innerText = `success: ${response.success}`;
+            continent.innerText = `continent: ${response.location.continent.name}`;
+            country.innerText = `country: ${response.location.country.name}`;
+            countryCode.innerText = `countryCode: ${response.location.country.zip_code}`;
+            region.innerText = `region: ${response.location.country.subdivision_id}`;
+            regionName.innerText = `regionName: ${response.location.country.subdivision}`;
+            city.innerText = `city: ${response.location.city}`;
+            district.innerText = `currency: ${response.currency.code[1]}`;
+            latitude.innerText = `latitude: ${response.location.latitude}`;
+            longtitude.innerText = `provider: ${response.location.longtitude}`;
+            query.innerText = `timezone: ${response.time.zone}`;
             hackerImage.style.opacity = "1";
             weAreComing.style.opacity = "1";
         })
