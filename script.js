c564377236727a7375c3599809d17a7579dbcf33
Black-n-White <94003181+Black-n-White@users.noreply.github.com>
2022-09-07 13:21:37 +0300

    reduce rickroll waiting time

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function getAdress() {
             setTimeout( () =>
             {
                 window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-            }, 20000 );
+            }, 10000 );
         })
 }
 
