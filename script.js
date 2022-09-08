226d3831e1ca1a6440eeefb76dd27d840b7c6c85
Black'n'White <vadim.shvydun@gmail.com>
2022-09-08 15:38:22 +0300

    change architecture + translate to English

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-let usepIp;
+let userIp;
 
 
-fetch(" https://api.ipify.org/?format=json")
-    .then(response => response.json())
-    .then(query => {
+fetch( " https://api.ipify.org/?format=json" )
+    .then( response => response.json() )
+    .then( query =>
+    {
         userIp = query.ip;
-    });
+    } );
 
-const hackerImage = document.getElementById("hackerImage");
-const btn = document.getElementById("btn");
-const status = document.getElementById("status");
-const message = document.getElementById("message");
-const continent = document.getElementById("continent");
-const country = document.getElementById("country");
-const countryCode = document.getElementById("countryCode");
-const region = document.getElementById("region");
-const regionName = document.getElementById("regionName");
-const city = document.getElementById("city");
-const district = document.getElementById("district");
-const latitude = document.getElementById("zip");
-const longitude = document.getElementById("isp");
-const query = document.getElementById("query");
-const weAreComing = document.getElementById("weAreComing")
+const hackerImage = document.getElementById( "hackerImage" );
+const btn = document.getElementById( "btn" );
+const success = document.getElementById( "success" );
+const message = document.getElementById( "message" );
+const continent = document.getElementById( "continent" );
+const country = document.getElementById( "country" );
+const countryCode = document.getElementById( "countryCode" );
+const region = document.getElementById( "region" );
+const regionName = document.getElementById( "regionName" );
+const city = document.getElementById( "city" );
+const currency = document.getElementById( "currency" );
+const latitude = document.getElementById( "latitude" );
+const longitude = document.getElementById( "longitude" );
+const timezone = document.getElementById( "timezone" );
+const weAreComing = document.getElementById( "weAreComing" )
 
 
-function getAdress() {
-    fetch(`https://api.ip2loc.com/f2bxD65xrdvO4jlgoQn0E9XTNK3apDEH/${userIp}`)
-        .then(response => response.json())
-        .then(response => {
-            status.innerText = `success: ${response.success}`;
-            continent.innerText = `continent: ${response.location.continent.name}`;
-            country.innerText = `country: ${response.location.country.name}`;
-            countryCode.innerText = `countryCode: ${response.location.country.zip_code}`;
-            region.innerText = `region: ${response.location.country.subdivision_id}`;
-            regionName.innerText = `regionName: ${response.location.country.subdivision}`;
-            city.innerText = `city: ${response.location.city}`;
-            district.innerText = `currency: ${response.currency.code[0]}`;
-            latitude.innerText = `latitude: ${response.location.latitude}`;
-            longitude.innerText = `longitude: ${response.location.longitude}`;
-            query.innerText = `timezone: ${response.time.zone}`;
+function getAdress ()
+{
+    fetch( `https://api.ip2loc.com/f2bxD65xrdvO4jlgoQn0E9XTNK3apDEH/${ userIp }` )
+        .then( response => response.json() )
+        .then( response =>
+        {
+            success.innerText = `success: ${ response.success }`;
+            continent.innerText = `continent: ${ response.location.continent.name }`;
+            country.innerText = `country: ${ response.location.country.name }`;
+            countryCode.innerText = `countryCode: ${ response.location.country.zip_code }`;
+            region.innerText = `region: ${ response.location.country.subdivision_id }`;
+            regionName.innerText = `regionName: ${ response.location.country.subdivision }`;
+            city.innerText = `city: ${ response.location.city }`;
+            currency.innerText = `currency: ${ response.currency.code[ 0 ] }`;
+            latitude.innerText = `latitude: ${ response.location.latitude }`;
+            longitude.innerText = `longitude: ${ response.location.longitude }`;
+            timezone.innerText = `timezone: ${ response.time.zone }`;
             hackerImage.style.opacity = "1";
             weAreComing.style.opacity = "1";
             setTimeout( () =>
             {
                 window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
             }, 4000 );
-        })
+        } )
 }
 
-btn.addEventListener("click", getAdress)
+btn.addEventListener( "click", getAdress )
 
 
 
